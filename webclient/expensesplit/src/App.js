94c131ff9996eac34d323d130bc205b64060093f
kevin-ye <kevin.ye32@gmail.com>
2018-05-09 22:49:35 -0700

    more UI

--- webclient/expensesplit/src/App.js
+++ webclient/expensesplit/src/App.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react'
-import { Menu, Grid, Divider, Segment, Container } from 'semantic-ui-react'
+import { Menu, Grid, Divider, Segment, Container, List, Button } from 'semantic-ui-react'
 
 class ESTitleBar extends Component {
   render() {
@@ -32,9 +32,16 @@ class ESSideBar extends Component {
 
   render() {
     return (
-      <Menu vertical>
+      <Menu vertical fluid pointing>
         <Menu.Item>
-          {this.props.eventName}
+          <h2>{this.props.eventName} </h2>
+        </Menu.Item>
+        <Divider />
+        <Menu.Item active color='green'>
+          View transactions
+        </Menu.Item>
+        <Menu.Item color='green'>
+          View Report
         </Menu.Item>
       </Menu>
     );
@@ -43,21 +50,63 @@ class ESSideBar extends Component {
 
 ESSideBar.propTypes = {
   eventName: PropTypes.string
-};
+}
+
+const TransactionItem = (props) => {
+  return (
+    <List.Item>
+      <List.Content floated='right'>
+        <Button>Edit</Button>
+      </List.Content>
+      <List.Content>
+        <List.Header>{props.itemHeader}</List.Header>
+        {props.itemDetail}
+      </List.Content>
+    </List.Item>
+  );
+}
+
+TransactionItem.propTypes = {
+  itemHeader: PropTypes.string,
+  itemDetail: PropTypes.string
+}
 
 class ESMainPanel extends Component {
+
+  constructor(props) {
+    super(props);
+    this.testListItems = [
+      { header: 'header1', detail: 'detail1' },
+      { header: 'header2', detail: 'detail2' },
+      { header: 'header3', detail: 'detail3' },
+      { header: 'header4', detail: 'detail4' },
+      { header: 'header5', detail: 'detail5' }
+    ];
+  }
+
+  refreshTransactions() {}
+
+  changeSelectedEvent(newID) {
+    this.selectedEvent = newID();
+  }
+
   render() {
+
     return (
       <Container fluid>
-        <Menu attached='top' tabular>
-          <Menu.Item name='Transactions' active={true}>
-          </Menu.Item>
-          <Menu.Item name='Splitting'>
-          </Menu.Item>
-        </Menu>
-
-        <Segment attached='bottom'>
-          <p>test</p>
+        <Segment>
+          <Menu fluid>
+            <Menu.Item>
+            <Button primary>Add</Button>
+            </Menu.Item>
+          </Menu>
+          <List celled verticalAlign='middle'>
+            {
+              this.testListItems.map((item, index) => 
+                <TransactionItem key={index} itemHeader={item.header} itemDetail={item.detail} />
+              )
+            }
+          </List>
         </Segment>
       </Container>
     );
